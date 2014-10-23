d7df37cb23e9e7f5458b0efb70ff07b2434747c2
Josh Lemer <joshlemer@gmail.com>
2014-10-23 17:27:02 -0500

    I dunno but I thougght it would be working

---
+++ diagonalize.m
@@ -0,0 +1,24 @@
+function result = diagonalize(A)
+        [m, n] = size(A)
+        eigenvalues = eig(A)
+        diagonalizable = true
+        similarity_matrix = []
+        already_used_eigenvalues = []
+        for ind = 1:size(eigenvalues, 1)
+            #if dim(e-space_eigenvalues(ind)) < count(eigenvalues,eigenvalues(ind))
+            if (n - rank(A - eigenvalues(ind) * eye(n)) < sum(eigenvalues == eigenvalues(ind)))
+                disp("This matrix is NOT diagonalizable")
+                diagonalizable = false
+                result = false
+                return
+            elseif sum(already_used_eigenvalues == eigenvalues(ind)) == 0
+                disp(similarity_matrix)
+                already_used_eigenvalues = [already_used_eigenvalues eigenvalues(ind)]
+                similarity_matrix = blkdiag(similarity_matrix, null(A - eigenvalues(ind) * eye(n)))              
+                disp("good")
+                
+            endif
+            
+            end
+        result = false
+        return
