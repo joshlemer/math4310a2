f230c45ce4bc20a5bb66545e5b448c2deb59ba1e
Josh Lemer <joshlemer@gmail.com>
2014-10-23 18:18:28 -0500

    Prettied it up

--- diagonalize.m
+++ diagonalize.m
@@ -1,24 +1,25 @@
-function result = diagonalize(A)
+function [diagonalizable, similarity_matrix, diagonal_matrix] = diagonalize(A)
         [m, n] = size(A)
         eigenvalues = eig(A)
         diagonalizable = true
         similarity_matrix = []
         already_used_eigenvalues = []
         for ind = 1:size(eigenvalues, 1)
-            #if dim(e-space_eigenvalues(ind)) < count(eigenvalues,eigenvalues(ind))
+            #if the dimension of the eigenspace < the eigenvalue's multiplicity
             if (n - rank(A - eigenvalues(ind) * eye(n)) < sum(eigenvalues == eigenvalues(ind)))
                 disp("This matrix is NOT diagonalizable")
                 diagonalizable = false
-                result = false
+                similarity_matrix = false
+                diagonal_matrix = false
                 return
+
+            #else if we haven't already looked at this eigenvalue
             elseif sum(already_used_eigenvalues == eigenvalues(ind)) == 0
-                disp(similarity_matrix)
                 already_used_eigenvalues = [already_used_eigenvalues eigenvalues(ind)]
-                similarity_matrix = [similarity_matrix, null(A - eigenvalues(ind) * eye(n))]              
-                disp("good")
-                
+                similarity_matrix = [similarity_matrix, null(A - eigenvalues(ind) * eye(n))]
             endif
-            
             end
-        result = similarity_matrix
+
+        disp("This matrix is diagonalizable")
+        diagonal_matrix = inv(similarity_matrix) * A * similarity_matrix
         return
